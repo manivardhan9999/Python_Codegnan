a697e6b689fa4dfdf59953a6811640f56c05a837
manivardhan9999 <140253994+manivardhan9999@users.noreply.github.com>
2026-08-13 17:14:46 +0530

    Add files via upload
    A variable in Python is a name used to store or refer to a value or object in memory. Python is dynamically typed, so we don't need to declare the data type of a variable explicitly. The type is determined automatically based on the value assigned to it.

---
+++ Variables.py
@@ -0,0 +1,86 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#Variables
+a = 10
+print(a)
+10
+print(A)
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    print(A)
+NameError: name 'A' is not defined. Did you mean: 'a'?
+name = "Mani"
+print(name)
+Mani
+Name = "Manivardhan"
+print(Name)
+Manivardhan
+5 = 10
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+a5 = 20
+print(a5)
+20
+a012345 = 30
+print(a012345)
+30
+if = 40
+SyntaxError: invalid syntax
+for = 30
+SyntaxError: invalid syntax
+first name = "Mani"
+SyntaxError: invalid syntax
+first_name = "Mani"
+print(first_name)
+Mani
+fname = "Mani"
+lname = "Vardhan"
+print(fname+lname)
+ManiVardhan
+print(fname,lname)
+Mani Vardhan
+print(fname+" "+lname)
+Mani Vardhan
+a=4,b=5
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+a=5;b=6
+print(a+b)
+11
+a,b = 2,3
+print(a,b)
+2 3
+a,d,c = 1,2,3
+print(a,b,c)
+1 3 3
+print(d)
+2
+a = 1,2,3,4
+print(a)
+(1, 2, 3, 4)
+a,b,c = 1,2 3,4,5
+SyntaxError: invalid syntax
+a = (3,4,5,6)
+>>> print(a)
+(3, 4, 5, 6)
+>>> a,b,c = (3,4,5)
+>>> print(a,b,c)
+3 4 5
+>>> w = 6
+>>> print(w)
+6
+>>> del w
+>>> print(w)
+Traceback (most recent call last):
+  File "<pyshell#41>", line 1, in <module>
+    print(w)
+NameError: name 'w' is not defined
+>>> Name ="Mani"
+>>> print(Name)
+Mani
+>>> NAME = "Mani"
+>>> print(NAME)
+Mani
+>>> q = 2
+>>> print(q)
+2
+>>> q
+2
